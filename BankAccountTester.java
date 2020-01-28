36560b3fd426009eda56c9923d6f756e69101c6f
Michael Drews <55116352+michaeldrews7@users.noreply.github.com>
2020-01-27 20:50:44 -0500

    commited banking project

---
+++ BankAccountTester.java
@@ -0,0 +1,29 @@
+package banking;
+
+/**
+A class to test the BankAccount class.
+*/
+public class BankAccountTester
+{
+/**
+   Tests the methods of the BankAccount class.
+   @param args not used
+*/
+public static void main(String[] args)
+{
+   //This line creates a new BankAccount object called harrysChecking with 0 balance
+	  BankAccount harrysChecking = new BankAccount();
+
+	  //This line calls the deposit method for harrysChecking
+	  //Note the deposit is for $2000
+   harrysChecking.deposit(2000);
+
+   //This line calls the withdrawal method for harrysChecking
+   //Note the withdrawal is for $500
+   harrysChecking.withdraw(500);
+
+   //This line calls the getBalance method and prints it     
+   System.out.println(harrysChecking.getBalance());
+   System.out.println("Expected: 1500");      
+}
+}
